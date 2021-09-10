85933bcd8bb2776d5274953572f73e769122e7a3
Anatoli Stoev <a.stoev@gmail.com>
2021-09-10 13:43:50 +0200

    Initial Commit Phpas

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '23eecd89a0df40f21d7e86e5c0bc813756fbe2cd',
+        'name' => 'hautelook/phpass',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'hautelook/phpass' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '23eecd89a0df40f21d7e86e5c0bc813756fbe2cd',
+            'dev_requirement' => false,
+        ),
+    ),
+);
